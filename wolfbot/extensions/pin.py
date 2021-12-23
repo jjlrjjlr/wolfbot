1559f3e17eedaf8277ad42aff4eae7ae8b44a8af
jjlrjjlr <mc.jjlr@tutanota.com>
2021-12-22 23:01:09 -0800

    Further pin command improvement

--- wolfbot/extensions/pin.py
+++ wolfbot/extensions/pin.py
@@ -5,89 +5,132 @@ import hashlib
 import utils
 import pin_db
 from datetime import datetime
+import traceback
 
 pin_plugin = lightbulb.Plugin('Pin')
 
 @pin_plugin.command
+@lightbulb.option('channel', 'ID of the channel the message to save is in. This must be used with the `Message` option.', required=False)
+@lightbulb.option('message', 'ID of the message to save. This must be used with the `Channel` option.', required=False)
 @lightbulb.command('pin', 'Save a message to the pins channel and to the pins database.')
 @lightbulb.implements(lightbulb.commands.SlashCommand)
 async def pin_command(ctx: lightbulb.context.Context) -> None:
-    embed = hikari.Embed(
-        title='Save Message',
-        description='''Save a message to the pins channel by
-            replying directly to the message you
-            want saved within the next 45 seconds.''',
-        color=hikari.colors.Color(0xf5c71a)
-    )
-    await ctx.respond(embed=embed)
-    try:
-        with ctx.bot.stream(hikari.events.MessageCreateEvent, timeout=45).filter(('author_id', ctx.author.id)) as stream:
-            event: hikari.events.MessageCreateEvent
-            async for event in stream:
-                if event.message.content == 'save':
-                    pin_db.save_to_database(ctx.bot, event.message.referenced_message)
+    if ctx.options.channel is not None and ctx.options.message is not None:
+        try:
+            message = await ctx.bot.rest.fetch_message(int(ctx.options.channel), int(ctx.options.message))
+            
+            pin_db.save_to_database(ctx.bot, message)
 
-                    await ctx.bot.rest.create_message(
-                        ctx.bot.d.settings.get_save_channel(ctx.guild_id.real),
-                        embed=build_message_embed(event.message.referenced_message, ctx.get_guild().id.real)
-                    )
+            await ctx.bot.rest.create_message(
+                ctx.bot.d.settings.get_save_channel(ctx.guild_id.real),
+                embed=build_message_embed(message, ctx.get_guild().id.real)
+            )
 
-                    await ctx.edit_last_response(
-                        embed=hikari.Embed(
-                            title='Saved',
-                            description='Message has been saved successfully.',
-                            color=hikari.Color(0x66FF00)
+            await ctx.respond(
+                embed=hikari.Embed(
+                    title='Saved',
+                    description=f'[Message]({message.make_link(ctx.get_guild().id)}) has been saved successfully.',
+                    color=hikari.Color(0x66FF00)
+                )
+            )
+        except Exception as e:
+            print(traceback.format_exc())
+            await ctx.respond(
+                embed=hikari.Embed(
+                    title='Error',
+                    description=e,
+                    color=hikari.Color(0x660000)
+                )
+            )
+    else:
+        await ctx.respond(
+            embed=hikari.Embed(
+                title='Save Message',
+                description='''Save a message to the pins channel by
+                    replying directly to the message you
+                    want saved within the next 45 seconds.''',
+                color=hikari.colors.Color(0xf5c71a)
+            )
+        )
+        try:
+            with ctx.bot.stream(hikari.events.MessageCreateEvent, timeout=45).filter(('author_id', ctx.author.id)) as stream:
+                event: hikari.events.MessageCreateEvent
+                async for event in stream:
+                    if event.message.content == 'save':
+                        pin_db.save_to_database(ctx.bot, event.message.referenced_message)
+
+                        await ctx.bot.rest.create_message(
+                            ctx.bot.d.settings.get_save_channel(ctx.guild_id.real),
+                            embed=build_message_embed(event.message.referenced_message, ctx.get_guild().id.real)
                         )
-                    )
-                
-                elif event.message.content == 'cancel':
-                    await ctx.edit_last_response(
-                        embed=hikari.Embed(
-                            title='Canceled',
-                            description='Pin canceled.',
-                            color=hikari.Color(0x660000)
+
+                        await ctx.edit_last_response(
+                            embed=hikari.Embed(
+                                title='Saved',
+                                description='Message has been saved successfully.',
+                                color=hikari.Color(0x66FF00)
+                            )
                         )
-                    )
-                
-                elif event.message.content not in [None, '']:
-                    message_location = event.message.content.split(':')
                     
-                    message = await ctx.bot.rest.fetch_message(message_location[0], message_location[1])
+                    elif event.message.content == 'cancel':
+                        await ctx.edit_last_response(
+                            embed=hikari.Embed(
+                                title='Canceled',
+                                description='Pin canceled.',
+                                color=hikari.Color(0x660000)
+                            )
+                        )
                     
-                    pin_db.save_to_database(ctx.bot, message)
+                    elif event.message.content not in [None, ''] and ':' in event.message.content:
+                        message_location = event.message.content.split(':')
+                        message: hikari.messages.Message
 
-                    await ctx.bot.rest.create_message(
-                        ctx.bot.d.settings.get_save_channel(ctx.guild_id.real),
-                        embed=build_message_embed(message, ctx.get_guild().id.real)
-                    )
+                        try:
+                            message = await ctx.bot.rest.fetch_message(message_location[0], message_location[1])
+                        except Exception as e:
+                            print(traceback.format_exc())
+                            await ctx.edit_last_response(
+                                embed=hikari.Embed(
+                                    title='Error',
+                                    description=e,
+                                    color=hikari.Color(0x660000)
+                                )
+                            )
+                        
+                        pin_db.save_to_database(ctx.bot, message)
 
-                    await ctx.edit_last_response(
-                        embed=hikari.Embed(
-                            title='Saved',
-                            description='Message has been saved successfully.',
-                            color=hikari.Color(0x66FF00)
+                        await ctx.bot.rest.create_message(
+                            ctx.bot.d.settings.get_save_channel(ctx.guild_id.real),
+                            embed=build_message_embed(message, ctx.get_guild().id.real)
                         )
-                    )
 
-                else:
-                    await ctx.edit_last_response(
-                        embed=hikari.Embed(
-                            title='Error',
-                            description='Invalid response.',
-                            color=hikari.Color(0x660000)
+                        await ctx.edit_last_response(
+                            embed=hikari.Embed(
+                                title='Saved',
+                                description='Message has been saved successfully.',
+                                color=hikari.Color(0x66FF00)
+                            )
                         )
-                    )
 
-                await event.message.delete()
+                    else:
+                        await ctx.edit_last_response(
+                            embed=hikari.Embed(
+                                title='Error',
+                                description='Invalid response.',
+                                color=hikari.Color(0x660000)
+                            )
+                        )
+
+                    await event.message.delete()
             
 
-    except TimeoutError:
-        timeout_embed = hikari.Embed(
-            title='Save Canceled',
-            description='45 seconds have elapsed, save canceled.',
-            color=hikari.Color(0xdb0000)
-        )
-        await ctx.edit_last_response(embed=timeout_embed)
+        except TimeoutError:
+            timeout_embed = hikari.Embed(
+                title='Save Canceled',
+                description='45 seconds have elapsed, save canceled.',
+                color=hikari.Color(0xdb0000)
+            )
+            await ctx.edit_last_response(embed=timeout_embed)
 
 
 def build_message_embed(message: hikari.messages.Message, guild_id: int) -> hikari.Embed:
@@ -117,7 +160,7 @@ def load(bot: lightbulb.BotApp) -> None:
     bot.add_plugin(pin_plugin)
 
 
-def _format_attachment_urls(attachments: []) -> []:
+def _format_attachment_urls(attachments: []) -> [str]:
     if len(attachments) > 0:
         urls = []
         for attachment in attachments:
