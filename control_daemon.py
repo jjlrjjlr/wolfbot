e23748dd5eb63d12ea1b23fb2d5c4f3abac80178
jjlrjjlr <mc.jjlr@tutanota.com>
2022-01-07 20:32:01 -0800

    Changes to control daemon

--- control_daemon.py
+++ control_daemon.py
@@ -1,16 +1,23 @@
-import logging
-import cmd
-import os
-import sys
+from prompt_toolkit import print_formatted_text, HTML, Application
+from prompt_toolkit.layout.containers import HSplit, Window
+from prompt_toolkit.layout.controls import FormattedTextControl
+from prompt_toolkit.layout.layout import Layout
+from prompt_toolkit.key_binding import KeyBindings
 
-class CommandDaemon(cmd.Cmd):
-    intro = 'WolfBot interactive control shell.\nUse help or ? for a list of commands.'
-    def do_restart(self, line):
-        os.execv(sys.executable, [sys.executable] + sys.argv)
+KEY_BINDINGS = KeyBindings()
 
-    def do_EOF(self, line) -> None:
-        sys.exit()
+root_container = HSplit([
+    Window(content=FormattedTextControl(text='Test 1 2 3 4 5'))
+])
 
+@KEY_BINDINGS.add('c-q')
+def exit(event) -> None:
+    (type(event))
+    event.app.exit()
+
+def main() -> None:
+    wolfbot_app = Application(key_bindings=KEY_BINDINGS, full_screen=True, layout=Layout(root_container))
+    wolfbot_app.run()
 
 if __name__ == '__main__':
-    CommandDaemon().cmdloop()
+    main()
